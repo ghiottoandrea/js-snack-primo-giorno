1a511b396905170ee094ba70e0854bfc2a1be467
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-16 15:43:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//Esercizio 1
+const number = 100
+console.log(number);//number
+
+//Esercizio 2
+const PI = 3.14
+console.log(PI);//number
+
+//Esercizio 2/B
+//const PI = 3.1416//number
+console.log(PI);//undefined perchè una CONST non può essere cambiata a differenza di LET
+
+//Esercizio 2/C
+const radius = 8//number
+const circle = (radius * 2 * PI)//number
+console.log(radius);
+
+//Esercizio 3
+let name = "Carlo"//string
+console.log(name);
+
+name = "Marco"//string
+console.log(name);
+
